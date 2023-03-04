e7dea903c996283c22ea6ccf195cbf59e6f75696
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-03-04 14:29:25 -0300

    fix: replace variant for variants

--- sections/World.jsx
+++ sections/World.jsx
@@ -16,7 +16,7 @@ const World = () => (
     >
       <TypingText title="| People on the World" textStyles="text-center" />
       <TitleText title={<>Track friends around you and invite them to play together  in the same world</>} textStyles="text-center" />
-      <motion.div variant={fadeIn('up', 'tween', 0.3, 1)} className="relative mt-[68px] flex w-full h-[550px]">
+      <motion.div variants={fadeIn('up', 'tween', 0.3, 1)} className="relative mt-[68px] flex w-full h-[550px]">
         <img src="/map.png" alt="map" className="w-full h-full object-cover" />
         <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
           <img src="/people-01.png" alt="people" className="w-full h-full" />
